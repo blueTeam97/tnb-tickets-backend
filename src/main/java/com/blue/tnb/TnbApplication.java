bf19836986c3813db9eacf6e7115928dd74f4856
Vanuta Mihai <mihai.vanuta@gmail.com>
2020-05-04 14:49:14 +0300

    Add Spring Boot Project

---
+++ src/main/java/com/blue/tnb/TnbApplication.java
@@ -0,0 +1,13 @@
+package com.blue.tnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnbApplication.class, args);
+	}
+
+}
